9c5c0694fadb7b79015ac883d08e569485951378
ABHIRAJ P  R <abhirajkarimpadam@gmail.com>
2018-08-10 15:50:27 +0530

    Create tets.php

---
+++ tets.php
@@ -0,0 +1,9 @@
+<?php
+header("Content-type: text/xml");
+echo "<?xml version='1.0' encoding='UTF-8'?>";
+echo "<Response>";
+echo "<Play>http://com.twilio.music.ambient.s3.amazonaws.com/gurdonark_-_Exurb.mp3</Play>";
+echo "<Play>http://com.twilio.music.ambient.s3.amazonaws.com/gurdonark_-_Exurb.mp3</Play>";
+echo "<Play>http://com.twilio.music.ambient.s3.amazonaws.com/gurdonark_-_Exurb.mp3</Play>";
+echo "<Redirect/>";
+echo "</Response>";
